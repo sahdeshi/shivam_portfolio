c178c9cf2d10af5c7f7a9c3fdbdc0a6fe56dc03f
Shivam Sahdev <sahdeshi@sheridancollege.ca>
2021-12-18 21:53:28 -0500

    size

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { ExperienceComponent } from './experience/experience.component';
 import { ProjectsComponent } from './projects/projects.component';
 import {AboutMeComponent} from './about-me/about-me.component';
 const routes: Routes = [
-  {path: 'aboutme', component: AboutMeComponent},
+  {path: '', component: AboutMeComponent},
   {path: 'education', component: EducationComponent},
   {path: 'experience', component: ExperienceComponent},
   {path: 'projects', component: ProjectsComponent}
